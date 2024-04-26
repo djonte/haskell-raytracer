b7864eb513c7502e9fc06f19d975798acc769025
Jonte <djonatanbolenius@gmail.com>
2024-04-26 12:08:53 +0200

    simple gradient image generation

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,21 @@
 module Main (main) where
 
-import Lib
+-- import Lib not needed yet?
+import Graphics.Image as I
 
 main :: IO ()
 main = do
-	print "test"
+  let grad_color :: Image VU RGB Double
+      grad_color = makeImageR VU (256, 256) (uncurry gradient) -- uncurry will transform gradient into (Int, Int) -> ...
+
+  writeImage "app/images/grad_color.png" grad_color
+
+
+gradient :: Int -> Int -> Pixel RGB Double
+gradient i j =
+  let 
+    (width, height) = (256, 256) -- width and height of image
+    r = fromIntegral i / (width - 1)
+    g = fromIntegral j / (height - 1)
+    b = r * g -- it follows that b <= 1 since r, g <= 1
+   in PixelRGB r g b
